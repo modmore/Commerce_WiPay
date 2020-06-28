eb757b3cb87cf2e0ab71776762439fbdb28c3f00
Mark Hamstra <hello@markhamstra.com>
2020-06-28 23:40:32 +0200

    Add properties to lexicon

--- core/components/commerce_wipay/lexicon/en/default.inc.php
+++ core/components/commerce_wipay/lexicon/en/default.inc.php
@@ -10,3 +10,10 @@ $_lang['commerce_wipay.developer_id'] = 'Developer ID';
 $_lang['commerce_wipay.developer_id_desc'] = 'Unique development identifier. Find the developer ID in your WiPay developer menu, or use "1" for the sandbox.';
 $_lang['commerce_wipay.merchant_key'] = 'Merchant Key';
 $_lang['commerce_wipay.merchant_key_desc'] = 'Used to verify transactions. Find the merchant key in your WiPay developer menu, or use "123" for the sandbox.';
+
+// Transaction properties
+$_lang['commerce.reasonCode'] = 'Reason Code';
+$_lang['commerce.reasonDescription'] = 'Reason Description';
+$_lang['commerce.responseCode'] = 'Response Code';
+$_lang['commerce.hash'] = 'Provided verification hash';
+$_lang['commerce.expected_hash'] = 'Expected verification hash';
