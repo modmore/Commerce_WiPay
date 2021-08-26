e6914b9fc55bfa901d8db59111bbbbeebbb89f52
digitalpenguin <murray@digitalpenguin.hk>
2021-08-26 11:30:50 +0800

    Build 2.0.0-pl for release

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -24,7 +24,7 @@ if (!defined('MOREPROVIDER_BUILD')) {
     define('PKG_NAME', 'Commerce_WiPay');
     define('PKG_NAMESPACE', 'commerce_wipay');
     define('PKG_VERSION', '2.0.0');
-    define('PKG_RELEASE', 'rc2');
+    define('PKG_RELEASE', 'pl');
 
     /* load modx */
     require_once dirname(__DIR__) . '/config.core.php';
